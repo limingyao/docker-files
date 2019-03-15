1914342668445981421a0e4759720541f63695cf
mingyaoli <mingyaoli@tencent.com>
2019-03-16 02:23:48 +0800

    update mysql

--- mysql/runner.sh
+++ mysql/runner.sh
@@ -5,5 +5,5 @@ if [ ! -d $HOME/.docker-data/mysql ];then
     touch $HOME/.docker-data/mysql/my.cnf
 fi
 
-docker run -d --name mysql5.7 -p 3306:3306 -e MYSQL_ROOT_PASSWORD=root \\
+docker run -d --name mysql5.7 -p 3306:3306 -e MYSQL_ROOT_PASSWORD=root \
 -v $HOME/.docker-data/mysql/my.cnf:/etc/mysql/my.cnf -v $HOME/.docker-data/mysql/data:/var/lib/mysql mysql:5.7
